6fcc8e94052620227049250654fb58917501ec29
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-03-17 10:07:09 +0330

    One Filed

--- scripts/simple_checks.py
+++ scripts/simple_checks.py
@@ -1,8 +1,23 @@
+import random, string
 from extras.scripts import Script
 from dcim.models import Device
+from tenancy.models import Tenant
 from virtualization.models import VirtualMachine
 from utilities.exceptions import AbortScript
 
+
+def generate_random_string(length:int=10,*,
+        ascii:bool = True,
+        digits:bool = True,
+        punctuation:bool = True,
+        ) -> str:
+
+        chars = ''
+        if ascii: chars += string.ascii_letters
+        if digits: chars += string.digits
+        if punctuation: chars += string.punctuation
+
+        return ''.join(random.choice(chars) for _ in range(length))
 
 class APIFriendlyIPAddress(Script):
     name = 'API Friendly IP Address'
@@ -79,3 +94,41 @@ class APIFriendlyIPAddress(Script):
                 machine.save()
 
             self.log_success(f'Address set: {ip_address}', machine)
+
+
+class GenerateSuppotToken(Script):
+    name = 'Generate Support Token'
+    description = 'Creates random generated token for tenants'
+    commit_default = True
+
+    def run(self, data: dict, commit: bool) -> None:
+        self.log_info(f'Commit mode: {'yes' if commit else 'no'}')
+
+        tenants = Tenant.objects.all()
+        if not tenants: self.log_failure('No tenants to preform operation'); return
+
+        self.log_info('Got all the tenants')
+
+        self.log_debug('Checking Custom fields: Started')
+        if 'support_token' not in tenants[0].cf:
+            raise AbortScript('Custom Field is not defined: support_token')
+
+        else:
+            self.log_debug('Checking Custom fields: Passed') 
+
+        for tenant in tenants:
+            if tenant.cf['support_token'] is not None:
+                self.log_info(f'Tenant already has token', tenant)
+                continue
+            
+            if commit:
+                self.log_debug(f'Generating token', tenant)
+                new_token =  generate_random_string(32, punctuation=False)
+                tenant.snapshot()
+                tenant.custom_field_data['support_token'] = new_token
+                tenant.full_clean()
+                tenant.save()
+                self.log_success(f'Created new token', tenant)
+
+            else:
+                self.log_warning(f'No Token Found', tenant)
