5741eface2f4495996d1b30eab26e06a9ddcba73
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-05-31 10:31:39 +0330

    Improved Login

--- scripts/zabbix_integeration.py
+++ scripts/zabbix_integeration.py
@@ -29,7 +29,11 @@ class ZabbixMixin:
         self.log_info(f'Initiated Zabbix Client - Version: {zabbix_version}')
 
         # Login
-        self.zabbix_client.login()
+        self.zabbix_client.login(
+            token=Zabbix_Config.get('token'),
+            user=Zabbix_Config.get('user'),
+            password=Zabbix_Config.get('password')
+            )
 
     def validate_device_custom_fields(self, devices) -> None:
         if not devices:
